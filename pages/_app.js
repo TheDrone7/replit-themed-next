4ef6d1047303cfc6fb906b131b26919a1f83862d
TheDrone7 <hsbhatia765@outlook.com>
2021-12-28 13:41:47 +0530

    Fixed stuff

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,10 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
-      <title>Monthly Repls</title>
+      <title>Replit themed website</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="description" content="The best repls created and shared on replit, each month!" />
-      <meta property="og:title" content="Monthly Repls" />
+      <meta name="description" content="My super cool app description!" />
+      <meta property="og:title" content="Replit themed website" />
     </Head>
     <Component {...pageProps} />
   </>)
